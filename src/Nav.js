3043a092a8e26f4adc5b4c3d95fb183a6cd6aed1
Abdulhamid Usman <hazemidoo@gmail.com>
2020-07-19 17:48:32 +0100

    stopped at links

---
+++ src/Nav.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+
+function Nav() {
+
+  const navStyle = {
+    color: 'white',
+    textDecoration: 'none'
+  };
+
+  return (
+    <nav>
+      <h3>logo</h3>
+      <ul className="nav-links">
+        <Link style={navStyle} to='/about'>
+          <li>About</li>
+        </Link>
+        <Link style={navStyle} to='/shop'>
+          <li>Shop</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
